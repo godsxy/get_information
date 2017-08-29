7b252bb99e2b963b336fb140ab5b397563396e73
godsxy <kawkarm@gmail.com>
2017-08-29 21:11:21 +0700

    make main file that call another function

---
+++ main.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+ThisFileDetail="""
+main.py ไฟล์หลักที่ใช้เริ่มการทำงาน
+by Sorapunya Insala
+"""
+
+from project_end.web.jobdb import getJobLink
+
+getJobLink
