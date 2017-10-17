2629801ed4de49d42408b8e3da0a138c38e4fcde
godsxy <kawkarm@gmail.com>
2017-10-18 03:01:31 +0700

    update query for level & detail remove ben

--- uploadData.py
+++ uploadData.py
@@ -14,14 +14,12 @@ def uploadToSql(jobs_detail):
         c = db.cursor()
         c.execute("SELECT VERSION()")
         data = c.fetchone()
-        #print("db version: ", data)
     except :
         print("connect error")
     for i in jobs_detail:
-        #sql='INSERT INTO main ( `j_name`, `cop_name`, `type`, `edu`, `loc`, `time`, `ben`, `jfunc`, `indus`) VALUES ("%s","%s","%s","%s","%s","%s","%s","%s","%s")'% (i["j_name"],i["cop_name"],i["type"],i["edu"],i["loc"],i["time"],i["ben"],i["jfunc"],i["indus"])
         try:
             c.execute("SET NAMES utf8mb4;")
-            c.execute("""INSERT INTO main ( `j_name`, `cop_name`, `type`, `edu`, `loc`, `time`, `ben`, `jfunc`, `indus`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""", (i["""j_name"""],i["""cop_name"""],i["""type"""],i["""edu"""],i["""loc"""],i["""time"""],i["""ben"""],i["""jfunc"""],i["""indus"""],))
+            c.execute("""INSERT INTO main (`j_name`, `cop_name`, `loc`, `detail`, `level`, `edu`, `type`, `jfunc`, `indus`, `time`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", (i["""j_name"""],i["""cop_name"""],i["""loc"""],i["""detail"""],i["""lv"""],i["""edu"""],i["""type"""],i["""jfunc"""],i["""indus"""],i["""time"""],))
             db.commit()
         except Exception as e:
             print("ลำดับข้อมูลอันที่ "+str(i['index'])+"มีปัญหา")
