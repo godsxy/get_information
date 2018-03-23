557095df4903f49c32e22281a1acf75c416c7310
Godsxy <kawkarm@gmail.com>
2018-03-23 11:15:28 +0700

    fix data from str to int improve code

--- web_file/ResultMap.php
+++ web_file/ResultMap.php
@@ -7,8 +7,11 @@
 		<title>Title Page</title>
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
@@ -26,12 +29,17 @@
 					$page = 1;
 				}
                 $MapName=$_GET['MapName'];
-                $sql = "SELECT `id`,`j_name`, `cop_name`,`time`, `loc`, `jfunc`, `indus` FROM `main` WHERE `out_date`=0 AND `loc`='$MapName'";
+								$SelectMapId = "SELECT `id` FROM `location` WHERE `name`='$MapName'";
+								$queryMapId = $conn->query($SelectMapId);
+								while($row = mysqli_fetch_assoc($queryMapId)){
+									$mapId = $row['id'];
+								}
+                $sql = "SELECT `id`,`j_name`, `cop_name`,`time`, `loc`, `jfunc`, `indus` FROM `main` WHERE curdate()<date_add(`time`,interval 8 day) AND `loc`='$mapId'";
                 $query = $conn->query($sql);
                 $NumCount = mysqli_num_rows($query);
                 $totalPage= ceil($NumCount / $pagelen);
                 $goto = ($page-1) * $pagelen;
-                $SelectData ="SELECT `id`,`j_name`, `cop_name`,`time`, `loc`, `jfunc`, `indus` FROM `main` WHERE `out_date`=0 AND `loc`='$MapName' ORDER BY `time` DESC,`jfunc` LIMIT $goto, $pagelen";
+                $SelectData ="SELECT `id`,`j_name`, `cop_name`,`time`, `loc`, `jfunc`, `indus` FROM `main` WHERE curdate()<date_add(`time`,interval 8 day) AND `loc`='$mapId' ORDER BY `time` DESC,`jfunc` LIMIT $goto, $pagelen";
                 $QueryData  = $conn->query($SelectData);
                 		if ($QueryData->num_rows > 0) {
 		?>
@@ -50,13 +58,25 @@
 					                <tbody>
 					                	<?php
 					                			while($row = mysqli_fetch_assoc($QueryData)) {
+																	$sqlCop = "SELECT `name` FROM `cop_name` WHERE `id`=".$row['cop_name'];
+																	$sqlJfunc = "SELECT `name` FROM `jfunc` WHERE `id`=".$row['jfunc'];
+																	$sqlLoc = "SELECT `name` FROM `location` WHERE `id`=".$row['loc'];
+																	$queryCop = $conn->query($sqlCop);
+																	$queryCop = mysqli_fetch_assoc($queryCop);
+																	$queryCop = $queryCop['name'];
+																	$queryFunc = $conn->query($sqlJfunc);
+																	$queryFunc = mysqli_fetch_assoc($queryFunc);
+																	$queryFunc = $queryFunc['name'];
+																	$queryLoc = $conn->query($sqlLoc);
+																	$queryLoc = mysqli_fetch_assoc($queryLoc);
+																	$queryLoc = $queryLoc['name'];
 					                	?>
 								                    <tr onclick="document.location = 'detail.php?id= <?php echo $row['id'] ?>';">
 									                    <td style='max-width: 250px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $row['j_name'] ?> </td>
-									                    <td style='max-width: 250px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $row['cop_name']?></td>
-									                    <td style='max-width: 100px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $row['jfunc']?></td>
+									                    <td style='max-width: 250px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $queryCop ?></td>
+									                    <td style='max-width: 100px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $queryFunc?></td>
 									                    <td style='max-width: 150px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $row['indus']?></td>
-									                    <td style='max-width: 90px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $row['loc']?></td>
+									                    <td style='max-width: 90px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $queryLoc?></td>
 									                    <td style='max-width: 90px;table-layout: fixed;word-wrap: break-word;cursor:pointer'><?php echo $row['time']?></td>
 								                    </tr>
 					<?php
@@ -73,7 +93,8 @@
 		 <?php
 		 	if ($page >1) {
 		 		$back = $page-1;?>
-		 		<a style='margin-left: 5px' href='<?php echo $PHP_SELF ?>?page=<?php echo $back ?>&MapName=<?php echo $MapName ?>'><span aria-hidden='true'>&laquo;</span></a>
+				<a style='margin-left: 5px' href='<?php echo $PHP_SELF ?>?page=<?php echo 1 ?>&MapName=<?php echo $MapName ?>'><span aria-hidden='true' class="glyphicon glyphicon-fast-backward"></span></a>
+				<a style='margin-left: 5px' href='<?php echo $PHP_SELF ?>?page=<?php echo $back ?>&MapName=<?php echo $MapName ?>'><span aria-hidden='true' class="glyphicon glyphicon-triangle-left"></span></a>
 		 	<?php }
 		 ?>
 		 </li>
@@ -82,16 +103,12 @@
 		 <?php
 		 	if($page < $totalPage) {
 		 	    $next = $page +1; ?>
-					<a style='margin-left: 5px' href='<?php echo $PHP_SELF ?>?page=<?php echo $next ?>&MapName=<?php echo $MapName ?>'><span aria-hidden='true'>&raquo;</span></a>
+					<a style='margin-left: 5px' href='<?php echo $PHP_SELF ?>?page=<?php echo $next ?>&MapName=<?php echo $MapName ?>'><span aria-hidden='true' class="glyphicon glyphicon-triangle-right"></span></a>
+					<a style='margin-left: 5px' href='<?php echo $PHP_SELF ?>?page=<?php echo $totalPage ?>&MapName=<?php echo $MapName ?>'><span aria-hidden='true' class="glyphicon glyphicon-fast-forward"></span></a>
 		 <?php	}
 		 ?>
 		 </li>
 		 </ul>
 		</div>
-		<!-- jQuery -->
-		<script src="//code.jquery.com/jquery.js"></script>
-		<!-- Bootstrap JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 	</body>
 </html>
