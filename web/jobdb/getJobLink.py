bc9746b33b2eadfb6c057584fe0e48fd56d31971
Godsxy <kawkarm@gmail.com>
2018-05-05 09:20:48 +0700

    update query delete overday

--- web/jobdb/getJobLink.py
+++ web/jobdb/getJobLink.py
@@ -15,33 +15,39 @@ def get_day(name):
     finalReslut=[]
     db=conDB.conDB()
     c = db.cursor()
-    sql = """ SELECT id FROM `jfunc` WHERE `name`=%s"""
-    sql2 = """ SELECT time FROM `main` WHERE `jfunc`=%s ORDER BY time DESC LIMIT 1"""
-    sql3 = """ SELECT COUNT(time) FROM `main` WHERE `time`=%s AND `jfunc`=%s"""
-    c.execute(sql,(name))
-    getID = c.fetchall()
-    getID=getID[0][0]
-    c.execute(sql2,(getID))
-    getTime = c.fetchall()
-    getTime=getTime[0][0]
-    getTime=datetime.combine(getTime, datetime.min.time())
-    timeNow=datetime.now()
-    timeNow=timeNow.replace(hour=0, minute=0,second=0,microsecond=0)
-    getTime=getTime.replace(hour=0, minute=0,second=0,microsecond=0)
-    reslut =timeNow - getTime
-    reslut=reslut.days
+    sqlGetIdJfunc = """ SELECT id FROM `jfunc` WHERE `name`=%s"""
+    sqlGetLastTime = """ SELECT time FROM `main` WHERE `jfunc`=%s ORDER BY time DESC LIMIT 1"""
+    while True:
+        getID = c.execute(sqlGetIdJfunc,(name))
+        if getID:
+            getID = c.fetchall()
+            getID=getID[0][0]
+            break
+        else:
+            sql4="INSERT INTO `jfunc`(`name`) VALUES (%s)"
+            c.execute(sql4,(name))
+            db.commit()
+            continue
+    while True:
+        getTime = c.execute(sqlGetLastTime,(getID))
+        if getTime:
+            getTime = c.fetchall()
+            getTime=getTime[0][0]
+            getTime=datetime.combine(getTime, datetime.min.time())
+            timeNow=datetime.now()
+            timeNow=timeNow.replace(hour=0, minute=0,second=0,microsecond=0)
+            getTime=getTime.replace(hour=0, minute=0,second=0,microsecond=0)
+            reslut =timeNow - getTime
+            reslut=reslut.days
+            break
+        else:
+            reslut=29
+            break
     if(reslut >= 29):
+        reslut=29
         finalReslut.append(reslut)
-        finalReslut.append(0)
     else:
-        reslut+=1
         finalReslut.append(reslut)
-        lastDay=datetime.now() - timedelta(reslut)
-        lastDay=lastDay.replace(hour=0, minute=0,second=0,microsecond=0)
-        c.execute(sql3,(lastDay,getID))
-        overDay = c.fetchall()
-        overDay=overDay[0][0]
-        finalReslut.append(overDay)
     return finalReslut
 
 ####################################
@@ -66,5 +72,5 @@ for i in jobs_links:
     print("ลิ้งอาชีพที่ทำอยู่ "+i +"อันที่ "+ str(index_page+1))
     day=get_day(jobs_date[index_page])
     print(day)
-    getPage.getPage(i,str(index_page+1),str(j),day[0],day[1])
+    getPage.getPage(i,str(index_page+1),str(j),day[0])
     index_page=index_page+1
